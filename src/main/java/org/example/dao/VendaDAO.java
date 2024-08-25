c5e7f5eed310b2131fef03d361e23dc74e3d8ee9
Thiago Gentil <thiagogentil499@gmail.com>
2024-08-25 22:23:34 +0000

    Fix: EntityManager

--- src/main/java/org/example/dao/VendaDAO.java
+++ src/main/java/org/example/dao/VendaDAO.java
@@ -1,65 +1,90 @@
 package org.example.dao;
 
-import org.example.models.Livro;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.example.util.HibernateUtil;
+import org.example.models.Venda;
+import org.example.util.JpaUtil;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import java.util.List;
 
-public class LivroDAO {
+public class VendaDAO {
 
-    public void salvar(Livro livro) {
-        Transaction transaction = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            session.save(livro);
+    public void salvar(Venda venda) {
+        EntityTransaction transaction = null;
+        EntityManager em = JpaUtil.getEntityManager();
+        try {
+            transaction = em.getTransaction();
+            transaction.begin();
+            em.persist(venda);
             transaction.commit();
         } catch (Exception e) {
-            if (transaction != null) {
+            if (transaction != null && transaction.isActive()) {
                 transaction.rollback();
             }
             e.printStackTrace();
+        } finally {
+            em.close();
         }
     }
 
-    public List<Livro> listarTodos() {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            return session.createQuery("from Livro", Livro.class).list();
+    public List<Venda> listarTodos() {
+        EntityManager em = JpaUtil.getEntityManager();
+        try {
+            return em.createQuery("from Venda", Venda.class).getResultList();
+        } finally {
+            em.close();
         }
     }
 
-    public Livro buscarPorId(Long id) {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            return session.get(Livro.class, id);
+    public Venda buscarPorId(Long id) {
+        EntityManager em = JpaUtil.getEntityManager();
+        try {
+            return em.find(Venda.class, id);
+        } finally {
+            em.close();
         }
     }
 
-    public void atualizar(Livro livro) {
-        Transaction transaction = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            session.update(livro);
+    public void atualizar(Venda venda) {
+        EntityTransaction transaction = null;
+        EntityManager em = JpaUtil.getEntityManager();
+        try {
+            transaction = em.getTransaction();
+            transaction.begin();
+            em.merge(venda);
             transaction.commit();
         } catch (Exception e) {
-            if (transaction != null) {
+            if (transaction != null && transaction.isActive()) {
                 transaction.rollback();
             }
             e.printStackTrace();
+        } finally {
+            em.close();
         }
     }
 
-    public void deletar(Livro livro) {
-        Transaction transaction = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            session.delete(livro);
+    public void deletar(Venda venda) {
+        EntityTransaction transaction = null;
+        EntityManager em = JpaUtil.getEntityManager();
+        try {
+            transaction = em.getTransaction();
+            transaction.begin();
+            if (em.contains(venda)) {
+                em.remove(venda);
+            } else {
+                Venda vendaManaged = em.find(Venda.class, venda.getId());
+                if (vendaManaged != null) {
+                    em.remove(vendaManaged);
+                }
+            }
             transaction.commit();
         } catch (Exception e) {
-            if (transaction != null) {
+            if (transaction != null && transaction.isActive()) {
                 transaction.rollback();
             }
             e.printStackTrace();
+        } finally {
+            em.close();
         }
     }
 }
