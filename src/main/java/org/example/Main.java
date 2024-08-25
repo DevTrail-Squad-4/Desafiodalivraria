2ee4ab9073153610324e3192a06ed80b6cb14b6a
DanielNaiff <danielnaiff344@gmail.com>
2024-08-25 16:27:42 -0300

    concertando a main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,39 +6,39 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        LivrariaVirtual livraria = new LivrariaVirtual();
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            System.out.println("Menu:");
-            System.out.println("1 - Cadastrar livro");
-            System.out.println("2 - Realizar uma venda");
-            System.out.println("3 - Listar livros");
-            System.out.println("4 - Listar vendas");
-            System.out.println("5 - Sair");
-
-            int opcao = scanner.nextInt();
-            scanner.nextLine();
-
-            switch (opcao) {
-                case 1:
-                    livraria.cadastrarLivro();
-                    break;
-                case 2:
-                    livraria.realizarVenda();
-                    break;
-                case 3:
-                    livraria.listarLivros();
-                    break;
-                case 4:
-                    livraria.listarVendas();
-                    break;
-                case 5:
-                    System.out.println("Saindo...");
-                    return;
-                default:
-                    System.out.println("Opção inválida.");
-            }
-        }
+//        LivrariaVirtual livraria = new LivrariaVirtual();
+//        Scanner scanner = new Scanner(System.in);
+//
+//        while (true) {
+//            System.out.println("Menu:");
+//            System.out.println("1 - Cadastrar livro");
+//            System.out.println("2 - Realizar uma venda");
+//            System.out.println("3 - Listar livros");
+//            System.out.println("4 - Listar vendas");
+//            System.out.println("5 - Sair");
+//
+//            int opcao = scanner.nextInt();
+//            scanner.nextLine();
+//
+//            switch (opcao) {
+//                case 1:
+//                    livraria.cadastrarLivro();
+//                    break;
+//                case 2:
+//                    livraria.realizarVenda();
+//                    break;
+//                case 3:
+//                    livraria.listarLivros();
+//                    break;
+//                case 4:
+//                    livraria.listarVendas();
+//                    break;
+//                case 5:
+//                    System.out.println("Saindo...");
+//                    return;
+//                default:
+//                    System.out.println("Opção inválida.");
+//            }
+//        }
     }
 }
